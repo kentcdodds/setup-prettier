57b5e3126809fe9bc0024a1572fd03a2790fd1d9
Kent C. Dodds <kent@doddsfamily.us>
2018-07-17 11:01:07 -0600

    things

---
+++ README.md
@@ -0,0 +1,3 @@
+# setup-prettier
+
+Just an example of how to setup prettier in your project
---
+++ src/index.js
@@ -0,0 +1,8 @@
+var one = 1
+
+console.log(one)
+
+console.log(
+  'hello                                                                   ',
+)
+;[1, 2, 3].forEach(() => {})
