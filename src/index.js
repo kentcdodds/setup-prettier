edc7abcab46ce565c67960ab04214f5e4ce49283
Kent C. Dodds <kent@doddsfamily.us>
2018-07-17 11:02:08 -0600

    things with bad formatting

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ var one = 1
 console.log(one)
 
 console.log(
-  'hello                                                                   ',
+  'hi and hello                                                                   ',
 )
 ;[1, 2, 3].forEach(() => {})
